29e1c239506232a67e12315f4229965812015bdb
Bill Erickson <bill.erickson@gmail.com>
2012-10-12 14:26:40 -0500

    fix plugin name

--- gallery-metabox.php
+++ gallery-metabox.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Gallery Metabox Norcross
+Plugin Name: Gallery Metabox
 Plugin URI: http://wordpress.org/extend/plugins/gallery-metabox/
 Description: Displays all the post's attached images on the Edit screen
 Author: Bill Erickson
