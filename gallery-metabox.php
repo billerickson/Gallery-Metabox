29b23a03957fc7bdf5dd36dc9313234e2342dd2b
Bill Erickson <bill.erickson@gmail.com>
2013-02-11 07:03:37 -0600

    Version bump for localization

--- gallery-metabox.php
+++ gallery-metabox.php
@@ -4,7 +4,7 @@ Plugin Name: Gallery Metabox
 Plugin URI: http://wordpress.org/extend/plugins/gallery-metabox/
 Description: Displays all the post's attached images on the Edit screen
 Author: Bill Erickson
-Version: 1.5
+Version: 1.5.1
 Author URI: http://www.billerickson.net
 */
 
